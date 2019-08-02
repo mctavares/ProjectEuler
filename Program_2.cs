515e4100225faa93a6082cb9910e04a86df11dd9
MT <mctavares@users.noreply.github.com>
2019-08-02 15:16:36 -0400

    Add files via upload
    Even Fibonacci numbers
    
    Problem 2
    Each new term in the Fibonacci sequence is generated by adding the previous two terms. By starting with 1 and 2, the first 10 terms will be:
    
    1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
    
    By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.

---
+++ Program_2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Explorer
+{
+    class Program
+    {
+        // https://projecteuler.net/problem=2
+        static IEnumerable<long> Fibonacci () 
+        {
+            long f0 = 0;
+            long f1 = 1;
+            long f;
+            while (true)
+            {
+                f = f0 + f1;
+                yield return f;
+                f0 = f1;
+                f1 = f;
+            }
+            
+        }
+        static void Main()
+        {
+            long sum = Fibonacci().TakeWhile(n => n < 4000000L).Where(n => n % 2 == 0).Sum();
+
+            Console.WriteLine("Sum: " + sum);
+        }
+
+
+    }
+       
+        
+}
+
